aa5c6b275ad6b3c3167a9a340894656d91228738
Masterlu22 <106290767+Masterlu22@users.noreply.github.com>
2026-08-23 14:00:50 -0300

    correção em relação a inicío da gameplay v3.1.1

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -7,6 +7,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
+	GameManager.reset()
 	get_tree().change_scene_to_file("res://scenes/cenario.tscn")
 
 func _on_quit_button_pressed() -> void:
